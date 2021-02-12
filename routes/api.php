959f1d0c6abfc0ad2218872b67cdd04ac3844dd5
easymagic <nnamware@yahoo.com>
2021-02-12 20:49:05 +0100

    np

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post('create-user',[\App\Http\Controllers\ApiCollectionController::class,'']);
+Route::post('create-user',[\App\Http\Controllers\ApiCollectionController::class,'createUser']);
+Route::post('get-user/{user}',[\App\Http\Controllers\ApiCollectionController::class,'getUser']);
+Route::post('get-file/{projectFile}',[\App\Http\Controllers\ApiCollectionController::class,'getFile']);
+Route::post('get-file-nodes',[\App\Http\Controllers\ApiCollectionController::class,'getFileNodes']);
+Route::post('get-file-images/{projectFile}',[\App\Http\Controllers\ApiCollectionController::class,'getFileImages']);
+Route::post('get-file-versions/{projectFile}',[\App\Http\Controllers\ApiCollectionController::class,'getFileVersions']);
+Route::post('get-image-fills/{projectFile}',[\App\Http\Controllers\ApiCollectionController::class,'getImageFills']);
+Route::post('get-comments/{project}',[\App\Http\Controllers\ApiCollectionController::class,'getComments']);
+Route::post('add-comment',[\App\Http\Controllers\ApiCollectionController::class,'addComment']);
+Route::post('get-me/{user}',[\App\Http\Controllers\ApiCollectionController::class,'getMe']);
+Route::post('get-users',[\App\Http\Controllers\ApiCollectionController::class,'getUsers']);
+Route::post('get-projects',[\App\Http\Controllers\ApiCollectionController::class,'getProjects']);
+Route::post('get-project-files/{project}',[\App\Http\Controllers\ApiCollectionController::class,'getProjectFiles']);
